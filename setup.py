c0b6d64f5175f5735d72719e8b3886717689685a
Erik Price <github@erikprice.net>
2015-11-22 11:24:07 -0800

    0.1.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='alexandra',
-    version='0.1.0',
+    version='0.1.1',
     description='Toolkit for writing Amazon Alexa skills as web services',
     author='Erik Price',
     url='https://github.com/erik/alexandra',

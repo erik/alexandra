29921e7ee53e10d99a33209edf14cc8039726733
Erik Price <erikprice@openmail.co>
2016-02-28 21:34:02 -0800

    Stub out alexandra.util tests

---
+++ tests/test_util.py
@@ -0,0 +1,34 @@
+from alexandra import util
+
+
+class TestRespond:
+    '''alexandra.util.respond'''
+
+    def test_sanity(self):
+        pass
+
+    def test_output_format(self):
+        pass
+
+    def test_reprompt(self):
+        pass
+
+    def test_reprompt_ssml(self):
+        pass
+
+
+class TestReprompt:
+    '''alexandra.util.reprompt'''
+
+    def test_reprompt_sanity(self):
+        pass
+
+
+class TestValidateTimestamp:
+    '''alexandra.util.validate_request_timestamp'''
+    pass
+
+
+class TestValidateCertificate:
+    '''alexandra.util.validate_request_certificate'''
+    pass
